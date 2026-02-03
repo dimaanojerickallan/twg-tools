af50778cff4c68d70349ebef68ea174ea9ef549c
dimaanojerickallan <dimaanojerickallan@gmail.com>
2026-02-03 21:38:21 +0800

    Update twg-tools.php
    Updated the code for disabling file editing

--- twg-tools.php
+++ twg-tools.php
@@ -30,4 +30,23 @@ add_filter('allow_minor_auto_core_updates', '__return_false');
 add_filter('allow_dev_auto_core_updates', '__return_false');
 
 // Disable theme & plugin editor
-add_filter('file_mod_allowed', '__return_false');
+add_action('admin_menu', function () {
+    remove_submenu_page('themes.php', 'theme-editor.php');
+    remove_submenu_page('plugins.php', 'plugin-editor.php');
+}, 999);
+
+add_action('admin_init', function () {
+    if ( ! is_admin() ) {
+        return;
+    }
+
+    $pagenow = $GLOBALS['pagenow'] ?? '';
+
+    if ( in_array($pagenow, ['theme-editor.php', 'plugin-editor.php'], true) ) {
+        wp_die(
+            __('File editing has been disabled on this site.'),
+            __('Access denied'),
+            ['response' => 403]
+        );
+    }
+});
